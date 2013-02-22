0d1a7000c7943e23aed206d739fe220a3fd912fb
Jesse Roy James II <malion1988@gmail.com>
2013-02-22 09:17:21 -0700

    Week 3 Project
    So as it turns out last week I may have done part of this weeks
    asignment.  Other than that my links seem to keep breaking when i get so
    far in... strange really.

---
+++ Week 3/James_Jesse_project3/js/main.js
@@ -0,0 +1,340 @@
+// Jesse James
+// 2-22-2013
+// VFW Project 3 1302
+//Wait for DOM to load
+window.addEventListener("DOMContentLoaded",function(){
+	//Global Variables
+	function $(x){
+		var elements = document.getElementById(x);
+		return elements;
+	};
+	var gameCatergoryTypes = ["Catergory", "Action", "Adventure", "First Person Shooter", "Racing","Role-Playing", "Strategy"];
+	//Create Game Catergory List
+	function makeCatergoryList(){
+		var addGameForm = document.getElementsByTagName("form");
+			gameCatergoryLi = $("gameCatergoryList");
+			makeElement = document.createElement("select");
+			makeElement.setAttribute("id","gameCatergory");
+		for(i=0; i<gameCatergoryTypes.length; i++){
+			var createOption = document.createElement("option");
+			var optionType = gameCatergoryTypes[i];
+			createOption.setAttribute("value", optionType);
+			createOption.innerHTML = optionType;
+			makeElement.appendChild(createOption);
+		}
+		gameCatergoryLi.appendChild(makeElement);
+	}
+	makeCatergoryList();
+	//Input ID's
+	var gameCatergory = $("gameCatergory");
+	var gameName = $("gameName");
+	var gamePublisher = $("gamePublisher");
+	var gameRelease = $("gameRelease");
+	var gameRate = $("gameRate");
+	var gameConsole = $("gameReviewForm").console;
+	var comments = $("comments");
+	var clearData = $("clearData");
+	var displayData = $("displayData");
+	var submitButton = $("submitButton");
+	var types = [gameCatergory, gameName, gamePublisher, gameRelease, gameRate, gameConsole, comments];
+	var types2 = ["Game Catergory: ", "Game Name: ", "Game Publisher: ", "Game Release: ", "Game Rate: ", "Game Console: ", "Comments: "];
+	//Toggle
+	function toggleDisplay(n) {
+		switch (n) {
+			case "on":
+				var back = document.createElement("a");
+				back.setAttribute("href", "additem.html");
+				back.setAttribute("id", "back");
+				document.body.appendChild(back);
+				back.innerHTML = "Back";
+				var clear = document.createElement("a");
+				clear.setAttribute("href", "#");
+				clear.setAttribute("id", "clearData2");
+				document.body.appendChild(clear);
+				clear.innerHTML = "Clear Data";
+				var displayClear = document.getElementById("clearData2");
+				displayClear.addEventListener("click", deleteLocalStorage);
+				$("addItemField").style.display = "none";
+				$("clearData").style.display = "none";
+				$("displayData").style.display = "none";
+				$("submitButton").hidden = "hidden";
+				$("addGameReview").innerHTML = "Game List";
+				break;
+			case "off":
+				var backButton = $("back");
+				var clearButton2 = $("clearData2");
+				document.body.removeChild(backButton);
+				document.body.removeChild(clearButton2);
+				$("addItemField").removeAttribute("style");
+				$("displayData").removeAttribute("style");
+				$("submitButton").removeAttribute("hidden");
+				$("addGameReview").innerHTML = "Add A Game Review";
+				$("clearData").style.display = "";
+				for(i=0; i<document.getElementsByClassName("displayDataList").length; i++){
+					document.getElementsByClassName("displayDataList")[i].style.display = "none";
+				}
+				break;
+			default:
+				return false;
+		};
+	};
+	//Populate form loop
+	var populateForm = function () {
+		for (i = 0; i<types2.length; i++) {
+			var key = types2[i];
+			var value = localStorage.getItem(key);
+			if (value != undefined) {
+				types[i].value = value;
+			};
+		};
+	};
+	// Get Value Functions
+	var getCatergory = function () {
+		localStorage.setItem("Game Catergory: ", gameCatergory.value);
+	};
+	var getName = function () {
+		localStorage.setItem("Game Name: ", gameName.value);
+	};
+	var getPublisher = function () {
+		localStorage.setItem("Game Publisher: ", gamePublisher.value);
+	};
+	var getRelease = function () {
+		localStorage.setItem("Game Release: ", gameRelease.value);
+	};
+	var getRate = function () {
+		var label = document.getElementById("ratingLabel");
+		label.innerHTML = "Game Rating(0-10): " + gameRate.value;
+		localStorage.setItem("Game Rate: ", gameRate.value);
+	};
+	var getConsole = function () {
+		var con = [];
+		for (i = 0; i < gameConsole.length; i++) {
+			if (gameConsole[i].checked) {
+				con.push(gameConsole[i].value);
+			};
+		};
+		localStorage.setItem("Game Console: ", con);
+		return (con);
+	};
+	var getComments = function () {
+		localStorage.setItem("Comments: ", comments.value);
+	};
+	//Form Validation
+	var formValid = function(){
+		if(gameCatergory.value == "" || gameCatergory.value == "Catergory"){
+			alert("Please enter a catergory!");
+			gameCatergory.focus();
+			return false;
+		}
+		if(gameName.value == ""){
+			alert("Please enter a game name!");
+			gameName.focus();
+			return false;
+		}
+		if(gamePublisher.value == ""){
+			alert("Please enter a Publisher!");
+			gamePublisher.focus();
+			return false;
+		}
+		if(gameRelease.value == ""){
+			alert("Please enter a release date!");
+			gameRelease.focus();
+			return false;
+		}
+		if(gameRate.value == ""){
+			alert("Please enter a rating!");
+			gameRate.focus();
+			return false;
+		}
+		if(gameConsole.xbox360.checked || gameConsole.ps3.checked || gameConsole.wii.checked){
+		}else{
+			alert("Please select a console!");
+			return false;
+		}
+		return true;
+	}
+	//Save Data
+	function saveGame() {
+		if(formValid() === true){
+			var newId = Math.floor(Math.random() * 1000000001);
+			var newType = {};
+			newType.catergory = ["Game Catergory: ", gameCatergory.value];
+			newType.name = ["Game Name: ", gameName.value];
+			newType.publisher = ["Game Publisher: ", gamePublisher.value];
+			newType.release = ["Game Release: ", gameRelease.value];
+			newType.rate = ["Game Rating: ", gameRate.value];
+			newType.console = ["Game Console: ", getConsole()];
+			newType.comments = ["Comments: ", comments.value];
+			localStorage.setItem(newId, JSON.stringify(newType));
+			alert(gameName.value + " Game Review Added!");
+			localStorage.removeItem("Game Catergory: ");
+			localStorage.removeItem("Game Name: ");
+			localStorage.removeItem("Game Publisher: ");
+			localStorage.removeItem("Game Release: ");
+			localStorage.removeItem("Game Rate: ");
+			localStorage.removeItem("Game Console: ");
+			localStorage.removeItem("Comments: ");
+			location.reload();
+			return;
+		};
+	};
+	//Edit Entry Function
+	function editThis() {
+		var myConfirm = confirm("Do you want to edit this contact?");
+		if(myConfirm === true){
+			toggleDisplay("off")
+			var thisValue = localStorage.getItem(this.key);
+			var thisObj = JSON.parse(thisValue);
+			gameCatergory.value = thisObj.catergory[1];
+			gameName.value = thisObj.name[1];
+			gamePublisher.value = thisObj.publisher[1];
+			gameRelease.value = thisObj.release[1];
+			gameRate.value = thisObj.rate[1];
+			//var gameConsoles = document.forms.gameReviewForm[0].console;
+			for(i=0; i<gameConsole.length; i++){
+				if(thisObj.console[1][i] === "Xbox 360"){
+					gameConsole[0].setAttribute("checked", "checked")
+				} else if(thisObj.console[1][i] === "Playstation 3") {
+					gameConsole[1].setAttribute("checked", "checked")
+				} else if(thisObj.console[1][i] === "Wii") {
+					gameConsole[2].setAttribute("checked","checked")
+				}
+			}
+			comments.value = thisObj.comments[1];
+			//localStorage.removeItem(this.key);
+			getRate();
+			submitButton.style.display = "none";
+			var editButton = document.createElement("input");
+			editButton.key = this.key;
+			editButton.setAttribute("id","editButton");
+			editButton.setAttribute("type","button");
+			editButton.setAttribute("value","Edit Review");
+			editButton.setAttribute("name","Edit Button");
+			document.body.appendChild(editButton);
+			editButton.addEventListener("click", editThisReview);
+		} else {
+			return;
+		};
+	};
+	//Edit review save data
+	function editThisReview(){
+		if(formValid() === true){
+			var newId = this.key
+			var newType = {};
+			newType.catergory = ["Game Catergory: ", gameCatergory.value];
+			newType.name = ["Game Name: ", gameName.value];
+			newType.publisher = ["Game Publisher: ", gamePublisher.value];
+			newType.release = ["Game Release: ", gameRelease.value];
+			newType.rate = ["Game Rating: ", gameRate.value];
+			newType.console = ["Game Console: ", getConsole()];
+			newType.comments = ["Comments: ", comments.value];
+			localStorage.setItem(newId, JSON.stringify(newType));
+			alert(gameName.value + " Game Review Edited!");
+			localStorage.removeItem("Game Catergory: ");
+			localStorage.removeItem("Game Name: ");
+			localStorage.removeItem("Game Publisher: ");
+			localStorage.removeItem("Game Release: ");
+			localStorage.removeItem("Game Rate: ");
+			localStorage.removeItem("Game Console: ");
+			localStorage.removeItem("Comments: ");
+			editButton.style.display = "none";
+			displayLocalStorage();
+			return;
+		}
+	}
+	//Delete Entry Function
+	function deleteThis() {
+		var thisConfirm = confirm("Are you sure you want to delete this entry?")
+		if(thisConfirm === true){
+			localStorage.removeItem(this.key);
+			alert("Please return to this page to see the changes you have made.");
+		} else {
+			return;
+		};
+	};
+	//Delete Local Storage
+	function deleteLocalStorage() {
+		if (localStorage.length === 0) {
+			alert("There is no data to clear!");
+		} else {
+			localStorage.clear();
+			alert("All data has been deleted");
+			window.location.reload();
+			return false;
+		}
+	}
+	function listLinks(key, thisLi){
+		//Delete Display Data Button
+		var deleteLink = document.createElement("a");
+		deleteLink.key = key;
+		deleteLink.setAttribute("href","#");
+		deleteLink.setAttribute("class","deleteEntry");
+		deleteLink.setAttribute("title","deleteEntry");
+		deleteLink.setAttribute("id","deleteEntry");
+		thisLi.appendChild(deleteLink);
+		deleteLink.innerHTML = "Delete Review";
+		//Edit Display Link
+		var editLink = document.createElement("a");
+		editLink.key = key;
+		editLink.setAttribute("href","#");
+		editLink.setAttribute("class","editEntry");
+		editLink.setAttribute("title","editEntry");
+		editLink.setAttribute("id","editEntry");
+		thisLi.appendChild(editLink);
+		editLink.innerHTML = "Edit Review";
+;
+	}
+	//Display Data
+	function displayLocalStorage() {
+		if (isNaN(localStorage.key(0)) || localStorage.length === 0) {
+			alert("There are no saved reviews!");
+			return;
+		};
+		toggleDisplay("on");
+		for (i=0; i<localStorage.length; i++) {
+			if(isNaN(localStorage.key(i))){
+			}else{
+				var key = localStorage.key(i);
+				var value = localStorage.getItem(key);
+				var newObj = JSON.parse(value);
+				var newDiv = document.createElement("ul");
+				var thisLi = document.createElement("li");
+				newDiv.setAttribute("id", "displayDataList");
+				newDiv.setAttribute("class", "displayDataList");
+				newDiv.setAttribute("display", "block");
+				document.body.appendChild(newDiv);
+				for (var o in newObj) {
+					var newLi = document.createElement("li");
+					newLi.setAttribute("class", o);
+					newDiv.appendChild(newLi);
+					var newText = newObj[o][0] + " " + newObj[o][1];
+					newLi.innerHTML = newText;
+					newDiv.appendChild(thisLi);
+				};
+				listLinks(key, thisLi);
+			};
+		};
+		for(i=0; i<document.getElementsByClassName("displayDataList").length; i++){
+					var eachDelete =document.getElementsByClassName("deleteEntry")[i];
+					var eachEdit = document.getElementsByClassName("editEntry")[i];
+					eachDelete.addEventListener("click", deleteThis);
+					eachEdit.addEventListener("click", editThis);
+					
+				}
+	};
+	//Populate past data
+	populateForm();
+	getRate();
+	//Event Listeners
+	gameCatergory.addEventListener("blur", getCatergory);
+	gameName.addEventListener("blur", getName);
+	gamePublisher.addEventListener("blur", getPublisher);
+	gameRelease.addEventListener("blur", getRelease);
+	gameRate.addEventListener("change", getRate);
+	submitButton.addEventListener("click", getConsole);
+	comments.addEventListener("blur", getComments);
+	//Button and Link Listners
+	clearData.addEventListener("click", deleteLocalStorage);
+	displayData.addEventListener("click", displayLocalStorage);
+	submitButton.addEventListener("click", saveGame);
+});
